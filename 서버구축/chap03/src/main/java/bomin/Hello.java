03d1be76016fe52c48d00f38a588b814ceac707a
bomikim0214 <rlaqhals05@naver.com>
2025-03-26 04:29:13 +0900

    push

---
+++ 서버구축/chap03/src/main/java/bomin/Hello.java
@@ -0,0 +1,6 @@
+package bomin;
+public class Hello {
+    public void say() {
+        System.out.println("안녕하세요");
+    }
+}
---
+++ 서버구축/chap03/src/main/java/bomin/Main.java
@@ -0,0 +1,7 @@
+package bomin;
+
+public class Main {
+    public static void main(String[] args) {
+        new Hello().say();
+    }
+}
